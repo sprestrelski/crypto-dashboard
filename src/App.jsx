f8cd3dec93b51e232b9b097c82007d6bc3a62f5e
Samantha Prestrelski <samantha.prestrelski@gmail.com>
2023-03-28 17:45:53 -0700

    lab 5 work

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import CoinInfo from "./Components/coinInfo";
+import SideNav from "./Components/sideNav";
+
+function App() {
+  const API_KEY = import.meta.env.VITE_CRYPTO_API_KEY;
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        `https://min-api.cryptocompare.com/data/all/coinlist?&api_key`
+        + API_KEY
+      );
+
+      const json = await response.json();
+      setList(json);
+    };
+
+    fetchAllCoinData().catch(console.error);
+  }, []);
+
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const searchItems = searchValue => {
+    setSearchInput(searchValue);
+    if (searchValue !== "") {
+      const filteredData = Object.keys(list.Data).filter((item) => 
+        Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+      )
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(Object.keys(list.Data));
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <div className="main">
+        <h1>crypto dashboard</h1>
+          <input
+            type="text"
+            placeholder="Search..."
+            onChange={(inputString) => searchItems(inputString.target.value)}
+          />
+          <ul>
+            {searchInput.length > 0
+              ? filteredResults.map((coin) => 
+                list.Data[coin].PlatformType === "blockchain" ? 
+                  <CoinInfo
+                    image={list.Data[coin].ImageUrl}
+                    name={list.Data[coin].FullName}
+                    symbol={list.Data[coin].Symbol}
+                  />
+                  : null
+                )
+              : list && Object.entries(list.Data).map(([coin]) => 
+                  list.Data[coin].PlatformType === "blockchain" ? 
+                  <CoinInfo
+                    image={list.Data[coin].ImageUrl}
+                    name={list.Data[coin].FullName}
+                    symbol={list.Data[coin].Symbol}
+                  />
+                  : null
+            )}
+          </ul>
+      </div>
+    
+      <SideNav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/sideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CryptoScam from "./cryptoScam"
+const SideNav = () => (
+  <div className="sideNav">
+    <h1>Crypto Scams</h1>
+    <CryptoScam/>
+  </div>
+);
+
+export default SideNav;
+
